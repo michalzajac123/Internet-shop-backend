f053cd8f97ca34ba9f211a5505ab4af3f10718a0
KiaCeed777 <mietla.maciek@gmail.com>
2025-04-28 09:40:48 +0200

    change type of imports to module

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const connectDB = async () => {
     try {
       await mongoose.connect(process.env.MONGO_URI);
